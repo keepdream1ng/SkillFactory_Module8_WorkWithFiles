919388e812e9ff6127e17e5edbba658d94975b85
Anatolii Ivanov <gotnickname@gmail.com>
2023-04-09 16:42:35 +0530

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    internal class DeleteOld
+    {
+        static int Main(string[] args)
+        {
+            string? inputPath;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Incert path to the folder for cleaning or use it as an argument next time you run programm.");
+                inputPath = Console.ReadLine();
+            }
+            // In case we suppose to use arguments.
+            else
+            {
+                inputPath = args[0];
+            }
+
+            if (!Directory.Exists(inputPath))
+            {
+                Console.WriteLine("Error: Directory you input does not exist!");
+                // Had to remember my C programming experience.
+                return 1;
+            }
+
+            string[] folders = Directory.GetDirectories(inputPath);
+            foreach (string folder in folders)
+            {
+                Console.WriteLine(Directory.GetCreationTime(folder));
+            }
+            Console.WriteLine(inputPath);
+            return 0;
+        }
+    }
+}
